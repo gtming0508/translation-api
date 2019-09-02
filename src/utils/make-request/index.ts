54f56ff48997a1e756bbb49d58bf7775788e1da8
jardenliu <15915839707@163.com>
2019-09-02 09:31:43 +0800

    fix: 🚗 support httpsProxyAgent

--- src/utils/make-request/index.ts
+++ src/utils/make-request/index.ts
@@ -4,93 +4,95 @@ import { request as requestHTTP } from 'http'
 import { request as requestHTTPs } from 'https'
 import { parse } from 'url'
 import { stringify } from 'querystring'
-const httpProxyAgent = require('http-proxy-agent')
+import httpProxyAgent from 'http-proxy-agent'
+import httpsProxyAgent from 'https-proxy-agent'
 
 import getError, { ERROR_CODE } from '../error'
 
-export default function (options: RequestOptions): Promise<any> {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-    const { method = 'get' } = options
-    const urlObj = parse(options.url, true)
-    const qs = stringify(Object.assign(urlObj.query, options.query))
+export default function(options: RequestOptions): Promise<any> {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+  const { method = 'get' } = options
+  const urlObj = parse(options.url, true)
+  const qs = stringify(Object.assign(urlObj.query, options.query))
 
-    const headers: StringObject = {
-        'User-Agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+  const headers: StringObject = {
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+  }
+
+  let body: string
+
+  if (method === 'post') {
+    switch (options.type) {
+      case 'form':
+        headers['Content-Type'] =
+          'application/x-www-form-urlencoded; charset=UTF-8'
+        body = stringify(options.body)
+        break
+
+      case 'json':
+      default:
+        headers['Content-Type'] = 'application/json; charset=UTF-8'
+        body = JSON.stringify(options.body)
+        break
     }
 
-    let body: string
+    headers['Content-Length'] = String(Buffer.byteLength(body))
+  }
 
-    if (method === 'post') {
-        switch (options.type) {
-            case 'form':
-                headers['Content-Type'] =
-                    'application/x-www-form-urlencoded; charset=UTF-8'
-                body = stringify(options.body)
-                break
+  Object.assign(headers, options.headers)
+  let agent = null
+  if ((<any>global).translateProxyUrl) {
+    const ProxyAgent: any =
+      urlObj.protocol === 'https:' ? httpsProxyAgent : httpProxyAgent
+    agent = new ProxyAgent((<any>global).translateProxyUrl)
+  }
+  const httpOptions = {
+    hostname: urlObj.hostname,
+    method,
+    path: urlObj.pathname + '?' + qs,
+    headers,
+    agent: agent,
+    auth: urlObj.auth
+  }
 
-            case 'json':
-            default:
-                headers['Content-Type'] = 'application/json; charset=UTF-8'
-                body = JSON.stringify(options.body)
-                break
+  const responseType = options.responseType || 'json'
+
+  return new Promise((resolve, reject) => {
+    const req = (urlObj.protocol === 'https:' ? requestHTTPs : requestHTTP)(
+      httpOptions,
+      (res: any) => {
+        // 内置的翻译接口都以 200 作为响应码，所以不是 200 的一律视为错误
+        if (res.statusCode !== 200) {
+          reject(getError(ERROR_CODE.API_SERVER_ERROR))
+          return
         }
 
-        headers['Content-Length'] = String(Buffer.byteLength(body))
-    }
-
-    Object.assign(headers, options.headers)
-    let agent = null
-    if ((<any>global).translateProxyUrl) {
-        agent = new httpProxyAgent((<any>global).translateProxyUrl);
-    }
-    console.log('agent: ', agent);
-    const httpOptions = {
-        hostname: urlObj.hostname,
-        method,
-        path: urlObj.pathname + '?' + qs,
-        headers,
-        agent: agent,
-        auth: urlObj.auth
-    }
-
-    const responseType = options.responseType || 'json'
-
-    return new Promise((resolve, reject) => {
-        const req = (urlObj.protocol === 'https:' ? requestHTTPs : requestHTTP)(
-            httpOptions,
-            res => {
-                // 内置的翻译接口都以 200 作为响应码，所以不是 200 的一律视为错误
-                if (res.statusCode !== 200) {
-                    reject(getError(ERROR_CODE.API_SERVER_ERROR))
-                    return
-                }
-
-                res.setEncoding('utf8')
-                let rawData = ''
-                res.on('data', (chunk: string) => {
-                    rawData += chunk
-                })
-                res.on('end', () => {
-                    // Node.js 端只支持 json，其余都作为 text 处理
-                    if (responseType === 'json') {
-                        try {
-                            resolve(JSON.parse(rawData))
-                        } catch (e) {
-                            // 与浏览器端保持一致，在无法解析成 json 时报错
-                            reject(getError(ERROR_CODE.API_SERVER_ERROR))
-                        }
-                    } else {
-                        resolve(rawData)
-                    }
-                })
-            }
-        )
-
-        req.on('error', e => {
-            reject(getError(ERROR_CODE.NETWORK_ERROR, e.message))
+        res.setEncoding('utf8')
+        let rawData = ''
+        res.on('data', (chunk: string) => {
+          rawData += chunk
         })
+        res.on('end', () => {
+          // Node.js 端只支持 json，其余都作为 text 处理
+          if (responseType === 'json') {
+            try {
+              resolve(JSON.parse(rawData))
+            } catch (e) {
+              // 与浏览器端保持一致，在无法解析成 json 时报错
+              reject(getError(ERROR_CODE.API_SERVER_ERROR))
+            }
+          } else {
+            resolve(rawData)
+          }
+        })
+      }
+    )
 
-        req.end(body)
+    req.on('error', (e: any) => {
+      reject(getError(ERROR_CODE.NETWORK_ERROR, e.message))
     })
+
+    req.end(body)
+  })
 }
